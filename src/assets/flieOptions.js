236a54d378ced4fa9d399bc829199c79c9c00f1b
nandu-boda <nanduboda13@gmail.com>
2026-03-15 12:39:25 +0530

    Update flieOptions.js

--- src/assets/flieOptions.js
+++ src/assets/flieOptions.js
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:5000";
+export const base_url = "https://foodfliebackend.onrender.com";
 export const googleMapsApiKey = "AIzaSyCD_DfIHXCu1NUeiSsq-m71IzRK9VZQ1sA";
