d8889c3e438ce5c1863787e794b827affa7684f3
nandu-boda <nanduboda13@gmail.com>
2026-03-18 10:17:03 +0530

    options changed

--- src/assets/flieOptions.js
+++ src/assets/flieOptions.js
@@ -1,4 +1,4 @@
-export const base_url = import.meta.env.BASE_URL || "";
-export const googleMapsApiKey = import.meta.env.GOOGLE_MAPS_API_KEY || "";
+export const base_url = process.env.BASE_URL || "";
+export const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY || "";
 
 console.log("Base URL:", base_url);
