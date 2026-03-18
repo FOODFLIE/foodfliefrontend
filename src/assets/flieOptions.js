e910db1c023394c8bd29cfc67ae8f0b3c45ef6a0
nandu-boda <nanduboda13@gmail.com>
2026-03-18 10:12:52 +0530

    removed meta

--- src/assets/flieOptions.js
+++ src/assets/flieOptions.js
@@ -1,2 +1,2 @@
-export const base_url = import.meta.env.VITE_BASE_URL || "";
-export const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY || "";
+export const base_url = import.meta.env.BASE_URL || "";
+export const googleMapsApiKey = import.meta.env.GOOGLE_MAPS_API_KEY || "";
