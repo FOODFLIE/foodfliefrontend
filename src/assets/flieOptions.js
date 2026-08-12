66f4703f38605e3926917fc8d93e0c08b60e8815
nandu-boda <nanduboda13@gmail.com>
2026-08-12 10:44:32 +0530

    feat: format code for consistency in flieOptions.js

--- src/assets/flieOptions.js
+++ src/assets/flieOptions.js
@@ -1,3 +1,6 @@
-export const base_url = import.meta.env.VITE_BASE_URL || "https://foodfliebackend.onrender.com";
-export const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY || "AIzaSyCD_DfIHXCu1NUeiSsq-m71IzRK9VZQ1sA";
-export const whatsappNumber = import.meta.env.WHATSAPP_NUMBER || "918309180145";
+export const base_url =
+  import.meta.env.VITE_BASE_URL || "https://foodfliebackend.onrender.com";
+export const googleMapsApiKey =
+  import.meta.env.VITE_GOOGLE_MAPS_API_KEY ||
+  "AIzaSyCD_DfIHXCu1NUeiSsq-m71IzRK9VZQ1sA";
+export const whatsappNumber = import.meta.env.WHATSAPP_NUMBER || "918096315194";
